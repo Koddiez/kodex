88d78b0e6daba7fca2224716190a82586c501b8a
Koddiez <stevendave847@gmail.com>
2025-07-23 23:47:51 -0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,37 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'kodex.dev'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
+      net: false,
+      tls: false,
     };
     return config;
   },
